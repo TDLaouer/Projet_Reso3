40e5e01a163ff8e347f10fc77d3bda8ae85a974c
MauvalM <37352368+MauvalM@users.noreply.github.com>
2018-06-05 13:54:54 +0200

    Add files via upload
    qzqzdzq

--- semantic.c
+++ semantic.c
@@ -4,10 +4,12 @@
 // Connection("connection-option")
 int Connection(char *p ){
 	_Token *r,*tok;
+	int res=200;;
 	Lnode *root;Lnode *node;
 	root=getRootTree();
 	if (p != NULL) {
 		r=searchTree(root,p);
+		if (r==NULL)return (200);
 		tok=r;
 		while(tok){
 
@@ -16,11 +18,12 @@ int Connection(char *p ){
 		if( strcmp(node->value ,"Connection") == 0 || strcmp( node->value , "Host")==0 || strcmp(node->value , "Content-Length")==0 || strcmp(node->value , "Accept-Encoding")==0 ||  strcmp(node->value , "Accept")==0 || strcmp(node->value , "User-Agent") ==0 || strcmp(node->value , "Referer")==0 || strcmp(node->value , "Transfer-Encoding")==0 || strcmp(node->value , "Cookie")==0){
 			return(400);
 		}
+		if (strncmp(node->value, "close",5)==0 ) res=1;
 		tok=tok->next;
 		}
-		
+
 	}
-return(200);
+return(res);
 }
 
 
@@ -31,7 +34,7 @@ int Connection2(char *p ){
 	Lnode *root;Lnode *node;
 	root=getRootTree();
 	if (p != NULL) {
-		
+
 		r=searchTree(root,p);
 		tok=r;
 		while(tok){
@@ -59,7 +62,6 @@ int BigHost(char* p){
 		r=searchTree(root,p);
 		if(r==NULL){return(0);}
 		tok=r;
-		printf("coucu\n");
 		node=(Lnode *)tok->node;
 		printf("%s --> FOUND [%.*s]\n",node->name,node->len,node->value);
 /*		purgeElement(&r);
@@ -74,6 +76,7 @@ int co(char* p){
 	root=getRootTree();
 	if (p != NULL) {
 		r=searchTree(root,p);
+		if(r==NULL)return(200);
 		tok=r;
 
 		node=(Lnode *)tok->node;
@@ -90,8 +93,8 @@ int host(char* p){
 	root=getRootTree();
 	if (p != NULL) {
 		r=searchTree(root,p);
-		
-		if(r==NULL){printf("cpiji\n");return(0);}
+
+		if(r==NULL){return(0);}
 		tok=r;
 		node=(Lnode *)tok->node;
 		printf("%s --> FOUND [%.*s]\n",node->name,node->len,node->value);
@@ -105,13 +108,13 @@ int originform(char* p){
 	_Token *r,*tok;
 	Lnode *root;Lnode *node;
 	root=getRootTree();
-	
+
 	if (p != NULL) {
 		r=searchTree(root,p);
-		
+
 		if(r==NULL)return(501);
 		tok=r;
-		
+
 		node=(Lnode *)tok->node;
 		printf("%s --> FOUND [%.*s]\n",node->name,node->len,node->value);
 	}
@@ -144,7 +147,7 @@ int version1(char *p){
 	root=getRootTree();
 	if (p != NULL) {
 		r=searchTree(root,p);
-		
+
 		tok=r;
 		node=(Lnode *)tok->node;
 		printf("%s --> FOUND [%.*s]\n",node->name,node->len,node->value);
@@ -173,7 +176,7 @@ int version2(char *p){
 	if( strncmp(node->value,"HTTP/1.1",8) != 0 && strncmp(node->value,"HTTP/1.0",8)!=0)return(505);
 	if(strncmp(node->value,"HTTP/1.1",8)==0){
 		ra=BigHost("Host");
-		
+
 		if(ra==0)return(400);
 		val=host("IP_litteral");
 		if(val!=0)return(501);
@@ -201,7 +204,7 @@ int method(char *p){
 		node=(Lnode *)tok->node;
 		printf("%s --> FOUND [%.*s]\n",node->name,node->len,node->value);
 		if(tok->next!=NULL)return(400);
-		
+
 if(strncmp(node->value,"GET",3)!=0 && strncmp(node->value, "HEAD",4)!=0 && strncmp(node->value , "POST", 4)!=0){return(501);}
 
 }
@@ -222,8 +225,6 @@ int contentlenght(char* p){
 		node=(Lnode *)tok->node;
 		printf("%s --> FOUND [%.*s]\n",node->name,node->len,node->value);
 		if(tok->next!=NULL)return(400);
-		
-		
 	}
 	if(TransferEnco("Transfer_Encoding") == 1){return(400);}
 return(200);
@@ -238,7 +239,7 @@ int TransferEnco(char *p){
 
 	if (p != NULL) {
 		r=searchTree(root,p);
-		if(r==NULL){return(1);}
+		if(r==NULL){return(2);}
 		tok=r;
 		if(tok->next!=NULL)return(400);
 		node=(Lnode *)tok->node;
@@ -281,7 +282,7 @@ return(200);
 }
 
 
-int lecturefich(char *monFichier, char* montype){
+int lecturefich(char *monFichier, char* montype, int len){
 	FILE* fichier;
 	char chaine[MAX]="";
 	fichier=fopen(monFichier,"r");
@@ -292,7 +293,7 @@ int lecturefich(char *monFichier, char* montype){
 	}
 	while(fgets(chaine, MAX, fichier) != NULL)
 	{
-		if(strcmp(montype,chaine) == 0){
+		if(strncmp(montype,chaine,len) == 0){
 			return(1);
 		}
 
@@ -306,6 +307,8 @@ int lecturefich(char *monFichier, char* montype){
 int mediarange(char* p, char* monfichier){
 	_Token *r,*tok; Lnode *node;
 	Lnode *root;
+			char * copie = malloc(MAX*sizeof(char));
+	int faux = 400;
 	root=getRootTree();
 	int oui=0;
 	int ra;
@@ -313,59 +316,54 @@ int mediarange(char* p, char* monfichier){
 		r=searchTree(root,p);
 		tok=r;
 		while(tok){
+			oui=0;
 		node=(Lnode *)tok->node;
 		printf("%s --> FOUND [%.*s]\n",node->name,node->len,node->value);
-		while(node->value[oui] != ';' || oui< node->len )oui++;
-
-		if(oui<node->len)node->value[oui]='\0';
-		ra=lecturefich(monfichier, node->value);
-		if(ra==-1)return(400);
+		while(node->value[oui] != ';' && oui < node->len )oui++;
+		strncpy(copie,node->value,oui);
+		ra=lecturefich(monfichier, node->value,oui);
+		if(ra==-1){return(faux);}
+		printf("Ici?\n");
 		tok=tok->next;
 		}
-		/*purgeElement(&r);
-		purgeTree(root);*/
 	}
 
 return(200);
 }
+
 int Verification(){
 	int ver;
 	int conn=200;
 	if( (ver=method("method")) != 200  ) return(ver);
-	
 	if( (ver=originform("origin_form")) != 200  ) return(ver);
-	
-
 	if( (ver=version2("HTTP_version")) != 200  ) return(ver);
-	printf("coidifnze\n");
 	if( (ver=cookie("Cookie")) != 200  ) return(ver);
-	printf("testé");
-	if( (ver=contentlenght("Content_Length")) != 200 && (ver!=1)){return(conn);}
-	if( (conn=TransferEnco("Transfer_Encoding")) != 200  &&  (conn!=1)){return(ver);}
-	if( (ver=mediarange("media_range","accept-types.txt") !=200 ))return(ver);
+	if( (ver=contentlenght("Content_Length")) != 200 && (ver!=1)){return(ver);}
+	if( (conn=Connection("connection_option")) != 200  && (conn !=1) ){return(conn);}
+	if( (ver=TransferEnco("Transfer_Encoding")) != 200 && (ver !=2)){return(ver);}
+	if( (ver=mediarange("media_range","accept-types.txt")) !=200){return(ver);}
 	/* Test si les header ne sont pas en double >>> Bad Request*/
-	if( (ver=co("Connection") != 200)) return(ver);
-	if( (ver=co("Referer") != 200)) return(ver);
-	if( (ver=co("User_Agent") != 200)) return(ver);
-	if( (ver=co("Host") != 200)) return(ver);
-	if( (ver=co("Accept") != 200)) return(ver);
-	if( (ver=co("Accept_Encoding") != 200)) return(ver);
+	if( (conn=co("Connection")) != 200) return(ver);
+	if( (ver=co("Referer")) != 200) return(ver);
+	if( (ver=co("User_Agent")) != 200) return(ver);
+	if( (ver=co("Host")) != 200) return(ver);
+	if( (ver=co("Accept")) != 200) return(ver);
+	if( (ver=co("Accept_Encoding")) != 200) return(ver);
 	return(conn);
 }
 
 
 char * returnInChar(int val){
-	printf("%d\n",val);
 	if (val == 501){
 		return "HTTP/1.1 501 Not Implemented\0";
 	}else if (val == 505){
 		return "HTTP/1.1 505 HTTP Version Not Supported\0";
 	}else if (val == 200){ // good, ok -> conncection keep-alive
-		return "HTTP/1.1 200 OK\r\nConnection: keep-alive\0";
+		return "HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n";
 	}else if (val == 1){ // good, ok -> connection close
-		return "HTTP/1.1 200 OK\r\nConnection: close\0";
+		return "HTTP/1.1 200 OK\r\nConnection: close\r\n";
 	}else{
 		return "HTTP/1.1 400 Bad Request\0";
 	}
-	
+
 }
